b36f825ef68beede149804b1fc2aac28ef2ba3c4
nebiyuelias1 <nebiyuelias1@gmail.com>
2024-05-17 09:10:49 +0200

    chore: Add command to modify diff in editor title menu

--- src/extension.ts
+++ src/extension.ts
@@ -19,7 +19,14 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from VS code!');
 	});
 
+	let modifyDiffDisposable = vscode.commands.registerCommand('lily-assistant.modifyDiff', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Another command executed!');
+	});
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(modifyDiffDisposable);
 }
 
 // This method is called when your extension is deactivated
